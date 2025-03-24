8f9b63c93434404b84eeba1904f0e375fdf8a359
muralikrishna2977 <159234648+muralikrishna2977@users.noreply.github.com>
2025-03-25 01:44:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -670,8 +670,10 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-server.listen(port, () => {
+
+server.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
+
 
 export { db };
